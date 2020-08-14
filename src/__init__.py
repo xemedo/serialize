4e6d8a9b3bf4301bf13b7228a7d13bffffdfa84d
NMO13 <martin.ennemoser@gmx.at>
2020-08-14 17:32:14 +0200

    Added __init__.py.

---
+++ src/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_restful import Api
+
+app = Flask(__name__)
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+
+    configure_app(app)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from src.views.product import ProductCreateView, ProductEntityView
+    api = Api(app)
+    api.add_resource(ProductCreateView, "/products/")
+    api.add_resource(ProductEntityView, "/products/<id>/")
+
+    return app
+
+def configure_app(app):
+    from src.config import Config
+    app.config.from_object(Config)
+
